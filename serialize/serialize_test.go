a23dca119c279311c9d501993e9a40434132a11a
donnie <donnie4w@gmail.com>
2023-09-14 00:21:38 +0800

    Update serialize_test.go

--- serialize/serialize_test.go
+++ serialize/serialize_test.go
@@ -151,3 +151,141 @@ func BenchmarkProtoBufDecode(b *testing.B) {
 		PDecode(bs, p)
 	}
 }
+
+func BenchmarkParallelMsgpackEncode(b *testing.B) {
+	p1 := TestBean{
+		Name:    "i哈哈aaaaaa12345",
+		Int64:   1 << 60,
+		Int32:   1 << 30,
+		Int16:   1 << 14,
+		Int8:    1 << 6,
+		ListVal: [][]byte{[]byte("i哈哈aaaaaa12345"), []byte("i哈哈bbbbbb12345"), []byte("i哈哈ccccccc12345")},
+		MapVal:  map[string][]byte{"aaaa": []byte("i哈哈aaaaaa123456"), "bbb": []byte("i哈哈bbbbbb123456"), "ccc": []byte("i哈哈ccccccc123456")},
+	}
+	// var bs []byte
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			msgpack.Marshal(p1)
+		}
+	})
+	// fmt.Println(string(bs))
+}
+
+func BenchmarkParallelMsgpackDecode(b *testing.B) {
+	p1 := TestBean{
+		Name:    "i哈哈aaaaaa12345",
+		Int64:   1 << 60,
+		Int32:   1 << 30,
+		Int16:   1 << 14,
+		Int8:    1 << 6,
+		ListVal: [][]byte{[]byte("i哈哈aaaaaa12345"), []byte("i哈哈bbbbbb12345"), []byte("i哈哈ccccccc12345")},
+		MapVal:  map[string][]byte{"aaaa": []byte("i哈哈aaaaaa123456"), "bbb": []byte("i哈哈bbbbbb123456"), "ccc": []byte("i哈哈ccccccc123456")},
+	}
+	bs, _ := msgpack.Marshal(p1)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			var p2 TestBean
+			msgpack.Unmarshal(bs, &p2)
+		}
+	})
+	// fmt.Println(p2)
+}
+
+func BenchmarkParallelGojsonEncode(b *testing.B) {
+	p1 := TestBean{
+		Name:    "i哈哈aaaaaa12345",
+		Int64:   1 << 60,
+		Int32:   1 << 30,
+		Int16:   1 << 14,
+		Int8:    1 << 6,
+		ListVal: [][]byte{[]byte("i哈哈aaaaaa12345"), []byte("i哈哈bbbbbb12345"), []byte("i哈哈ccccccc12345")},
+		MapVal:  map[string][]byte{"aaaa": []byte("i哈哈aaaaaa123456"), "bbb": []byte("i哈哈bbbbbb123456"), "ccc": []byte("i哈哈ccccccc123456")},
+	}
+	// var bs []byte
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			json.Marshal(p1)
+		}
+	})
+	// fmt.Println(string(bs))
+}
+
+func BenchmarkParallelGoJsonDecode(b *testing.B) {
+	p1 := TestBean{
+		Name:    "i哈哈aaaaaa12345",
+		Int64:   1 << 60,
+		Int32:   1 << 30,
+		Int16:   1 << 14,
+		Int8:    1 << 6,
+		ListVal: [][]byte{[]byte("i哈哈aaaaaa12345"), []byte("i哈哈bbbbbb12345"), []byte("i哈哈ccccccc12345")},
+		MapVal:  map[string][]byte{"aaaa": []byte("i哈哈aaaaaa123456"), "bbb": []byte("i哈哈bbbbbb123456"), "ccc": []byte("i哈哈ccccccc123456")},
+	}
+	bs, _ := json.Marshal(p1)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			var p2 TestBean
+			json.Unmarshal(bs, &p2)
+		}
+	})
+	// fmt.Println(p2)
+}
+
+func BenchmarkParallelThriftEncode(b *testing.B) {
+	bean := &Bean{Name: "i哈哈aaaaaa12345", LongVal: 1 << 60, IntVal: 1 << 30, Int16Val: 1 << 14, Int8Val: 1 << 6}
+	bean.ListVal = [][]byte{[]byte("i哈哈aaaaaa12345"), []byte("i哈哈bbbbbb12345"), []byte("i哈哈ccccccc12345")}
+	bean.MapVal = map[string][]byte{"aaaa": []byte("i哈哈aaaaaa123456"), "bbb": []byte("i哈哈bbbbbb123456"), "ccc": []byte("i哈哈ccccccc123456")}
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			TEncode(bean)
+		}
+	})
+}
+
+func BenchmarkParallelThriftDecode(b *testing.B) {
+	bean := &Bean{Name: "i哈哈aaaaaa12345", LongVal: 1 << 60, IntVal: 1 << 30, Int16Val: 1 << 14, Int8Val: 1 << 6}
+	bean.ListVal = [][]byte{[]byte("i哈哈aaaaaa12345"), []byte("i哈哈bbbbbb12345"), []byte("i哈哈ccccccc12345")}
+	bean.MapVal = map[string][]byte{"aaaa": []byte("i哈哈aaaaaa123456"), "bbb": []byte("i哈哈bbbbbb123456"), "ccc": []byte("i哈哈ccccccc123456")}
+	bs := TEncode(bean)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			TDecode(bs, &Bean{})
+		}
+	})
+}
+
+func BenchmarkParallelProtoBufEncode(b *testing.B) {
+	name := "i哈哈aaaaaa12345"
+	i64, i32 := int64(1<<60), int32(1<<30)
+	p1 := &PBean{
+		Name:     &name,
+		Int64Val: &i64,
+		Int32Val: &i32,
+		ListVal:  [][]byte{[]byte("i哈哈aaaaaa12345"), []byte("i哈哈bbbbbb12345"), []byte("i哈哈ccccccc12345")},
+		MapVal:   map[string][]byte{"aaaa": []byte("i哈哈aaaaaa123456"), "bbb": []byte("i哈哈bbbbbb123456"), "ccc": []byte("i哈哈ccccccc123456")},
+	}
+	// var bs []byte
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			PEncode(p1)
+		}
+	})
+	// fmt.Println(len(bs))
+}
+
+func BenchmarkParallelProtoBufDecode(b *testing.B) {
+	name := "i哈哈aaaaaa12345"
+	i64, i32 := int64(1<<60), int32(1<<30)
+	p1 := &PBean{
+		Name:     &name,
+		Int64Val: &i64,
+		Int32Val: &i32,
+		ListVal:  [][]byte{[]byte("i哈哈aaaaaa12345"), []byte("i哈哈bbbbbb12345"), []byte("i哈哈ccccccc12345")},
+		MapVal:   map[string][]byte{"aaaa": []byte("i哈哈aaaaaa123456"), "bbb": []byte("i哈哈bbbbbb123456"), "ccc": []byte("i哈哈ccccccc123456")},
+	}
+	bs, _ := PEncode(p1)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			PDecode(bs, &PBean{})
+		}
+	})
+}
